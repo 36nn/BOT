ccff35d694422fca691eda8dddc9c8bf74f790c8
online-edu-mirea-ru <zz09062006zz@gmail.com>
2026-04-15 22:16:54 +0300

    postgres connected

--- app/database/models.py
+++ app/database/models.py
@@ -1,61 +1,52 @@
-from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.orm import relationship, DeclarativeBase
-from sqlalchemy.ext.asyncio import AsyncAttrs, create_async_engine, async_sessionmaker, AsyncSession
+import os
+from sqlalchemy import BigInteger, String, ForeignKey
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+# фикс для render
+DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql+asyncpg://")
+
+engine = create_async_engine(DATABASE_URL, echo=True)
+async_session = async_sessionmaker(engine, expire_on_commit=False)
 
 
-# 🔹 БАЗА
-class Base(AsyncAttrs, DeclarativeBase):
+class Base(DeclarativeBase):
     pass
 
 
-engine = create_async_engine("sqlite+aiosqlite:///db.sqlite3")
-async_session = async_sessionmaker(engine, class_=AsyncSession)
-
-
-# 🔹 ПОЛЬЗОВАТЕЛЬ
 class User(Base):
     __tablename__ = "users"
 
-    id = Column(Integer, primary_key=True)
-    tg_id = Column(Integer, unique=True)
+    id: Mapped[int] = mapped_column(primary_key=True)
+    tg_id: Mapped[int] = mapped_column(BigInteger)
 
-    workouts = relationship(
-        "Workout",
-        back_populates="user",
-        cascade="all, delete-orphan"
-    )
+    workouts = relationship("Workout", back_populates="user")
 
 
-# 🔹 ТРЕНИРОВКА
 class Workout(Base):
     __tablename__ = "workouts"
 
-    id = Column(Integer, primary_key=True)
-    name = Column(String)
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String)
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
 
-    user_id = Column(Integer, ForeignKey("users.id"))
     user = relationship("User", back_populates="workouts")
-
-    exercises = relationship(
-        "Exercise",
-        back_populates="workout",
-        cascade="all, delete-orphan"
-    )
+    exercises = relationship("Exercise", back_populates="workout")
 
 
-# 🔹 УПРАЖНЕНИЕ
 class Exercise(Base):
     __tablename__ = "exercises"
 
-    id = Column(Integer, primary_key=True)
-    name = Column(String)
-    reps = Column(Integer)
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String)
+    reps: Mapped[int] = mapped_column()
+    workout_id: Mapped[int] = mapped_column(ForeignKey("workouts.id"))
 
-    workout_id = Column(Integer, ForeignKey("workouts.id"))
     workout = relationship("Workout", back_populates="exercises")
 
 
-# 🔥 СОЗДАНИЕ ТАБЛИЦ
 async def async_main():
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
